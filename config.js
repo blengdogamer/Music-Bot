83b3afcfe54286241ee72ae6ff333aa2444eec67
blengdogamer <40428498+blengdogamer@users.noreply.github.com>
2019-04-30 03:00:10 +0300

    Update config.js

--- config.js
+++ config.js
@@ -2,4 +2,4 @@ exports.TOKEN = 'NTcyNTcyMDI5MzU0NzcwNDM0.XMePTg.8nE-GMX7xtZiFNp2Br8V8CHv5Ag';
 
 exports.PREFIX = 'L';
 
-exports.GOOGLE_API_KEY = '';
+exports.GOOGLE_API_KEY = 'AIzaSyAdORXg7UZUo7sePv97JyoDqtQVi3Ll0b8';
